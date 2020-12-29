1aa59faf5ec1549b6776f84d245607a55feecabf
antim098 <40453155+antim098@users.noreply.github.com>
2020-12-29 20:01:39 +0530

    added base kafka trigger

--- src/main/java/com/trigger/TriggerThread.java
+++ src/main/java/com/trigger/TriggerThread.java
@@ -87,7 +87,7 @@ public class TriggerThread implements Callable<Object> {
         try {
             if (Trigger.getKafkaStatus()) {
                 //fileWriter.write("\n" + value);
-                producer.send(record);
+                //producer.send(record);
                 //producer.flush();
             } else {
                 //Sending records to file in case kafka is down.
